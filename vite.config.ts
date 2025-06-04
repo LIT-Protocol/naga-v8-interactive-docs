634b03b4647758e52f6cd61dc5a82e8c1c8ec0c2
Anson <ansonox@gmail.com>
2025-06-04 18:47:28 +0100

    this version works

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-    },
-  },
-  optimizeDeps: {
-    include: ['buffer'],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        globals: {
-          buffer: 'Buffer',
-        },
-      },
-    },
-  },
+  plugins: [react()]
 });
